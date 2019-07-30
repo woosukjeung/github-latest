a16acf6b1875e7af590f96baf5203ea700605a1c
woosukjeung <47073569+woosukjeung@users.noreply.github.com>
2019-07-29 22:55:35 -0700

    activity 6 submission

--- main.py
+++ main.py
@@ -6,15 +6,13 @@ import requests
 # Use Like python githubber.py JASchilz
 # (or another user name)
 
+
+
 if __name__ == "__main__":
     username = sys.argv[1]
 
-    # TODO:
-    #
-    # 1. Retrieve a list of "events" associated with the given user name
-    # 2. Print out the time stamp associated with the first event in that list.
+    response = requests.get("https://api.github.com/users/{}/events".format(username))
+    events = json.loads(response.content)
 
-    print("COMPLETE THE TODOs")
-    
-
+    print(events[0]['created_at'])
 
